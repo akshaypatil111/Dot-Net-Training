b72f037e15c35afa94206f3063e85ffcbb5de8b5
Akshay Patil <aksh.patil3@gmail.com>
2022-06-10 16:58:05 +0530

    commit message

--- Angular/first-sample-project/src/app/app.component.ts
+++ Angular/first-sample-project/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Customer } from './app.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'first-sample-project';
+  myname = 'vikash Verma'
+  CustomerModel: Customer = new Customer();
+  CustmerModels: Array<Customer> = new Array<Customer>();
+  AddCustomer() {
+    this.CustmerModels.push(this.CustomerModel);
+    this.CustomerModel = new Customer();
+  }
+  EditCustomer(input: Customer) {
+    console.log(input);
+  }
+  DeleteCustomer(input: Customer) {
+    console.log(input);
+  }
+
 }
