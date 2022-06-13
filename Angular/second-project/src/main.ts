c64ca29ed38bb2d86c14623f077a25c3d67c04f5
Akshay Patil <aksh.patil3@gmail.com>
2022-06-13 17:14:51 +0530

    commit message

--- Angular/second-project/src/main.ts
+++ Angular/second-project/src/main.ts
@@ -1,13 +1,12 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { MasterModule } from './app/home/master.module';
 
-
+import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(MasterModule)
+platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
