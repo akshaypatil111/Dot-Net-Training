3939bc38606d527ddfa6501a59e6e5f114f94f26
Akshay Patil <aksh.patil3@gmail.com>
2022-06-20 10:47:14 +0530

    commit message

--- Angular/iCart/src/app/app-routing.module.ts
+++ Angular/iCart/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
 
-@NgModule({
+
+const routes: Routes = [
+
+];
+
+@NgModule({ 
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
--- Angular/iCart/src/app/app.module.ts
+++ Angular/iCart/src/app/app.module.ts
@@ -4,21 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminComponent } from './Component/admin/admin.component';
-import { ProductComponent } from './Component/product/product.component';
 import { UserComponent } from './Component/user/user.component';
+import { ProductComponent } from './Component/product/product.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedService } from './shared.service';
+
+import {HttpClientModule} from '@angular/common/http'
+import { FormsModule,ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdminComponent,
+    UserComponent,
     ProductComponent,
-    UserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [SharedService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
