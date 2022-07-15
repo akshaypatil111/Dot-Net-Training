3d65c139c42319fcbcbf97360c614563aa7f194b
Akshay Patil <aksh.patil3@gmail.com>
2022-07-15 14:05:53 +0530

    commit message

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using Azure;
+using Azure.Data.Tables;
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var client = new TableClient(new Uri("https://levelll.table.core.windows.net"), "table1", new TableSharedKeyCredential("levelll", "3A3CSahUbol4nV8pIy+6U6DXi3V5rvB80UJq7KGBnyVATxzsGWktzsp98S+NGqlFWP546L8TIEKO+AStIpzZ/w=="));
+            Pageable<TableEntity> queryableTable = client.Query<TableEntity>();
+            foreach (TableEntity item in queryableTable)
+            {
+                Console.WriteLine(item.GetString("Name"));
+            }
+        }
+    }
+}
---
+++ EshoppingAPI/Model/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace EshoppingAPI.Model
+{
+    public partial class Category
+    {
+        public int Id { get; set; }
+        public string CatName { get; set; }
+    }
+}
