54cc5bd0e527259d3a000f42cf30e992923b5e33
Akshay Patil <aksh.patil3@gmail.com>
2022-07-17 00:45:16 +0530

    commit message

--- Sprint 2/EshoppingApp/src/app/app-routing.module.ts
+++ Sprint 2/EshoppingApp/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { CartComponent } from './cart/cart.component';
+import { FinalOrderComponent } from './finalorder/finalorder.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { OrdercompleteComponent } from './ordercomplete/ordercomplete.component';
 import { ProductComponent } from './product/product.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   {
@@ -9,9 +15,45 @@ const routes: Routes = [
     component: HomeComponent
   },
   {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
     path: 'product',
     component: ProductComponent
   },
+
+  {
+    path: 'admin',
+    component: AdminComponent
+  },
+  {
+    path: 'cart',
+    component: CartComponent
+  },
+ 
+
+  {
+    path:'finalorder',
+    component:FinalOrderComponent
+  },
+  {
+    path:'ordercomplete',
+    component:OrdercompleteComponent
+  },
+
+
+
+  
+ 
 ];
 
 @NgModule({
--- Sprint 2/EshoppingApp/src/app/app.module.ts
+++ Sprint 2/EshoppingApp/src/app/app.module.ts
@@ -4,17 +4,29 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { CartComponent } from './cart/cart.component';
 import { ProductComponent } from './product/product.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AuthService } from './services/auth.service';
+import { AdminComponent } from './admin/admin.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { FinalOrderComponent } from './finalorder/finalorder.component';
+import { MyorderComponent } from './myorder/myorder.component';
+import { CartComponent } from './cart/cart.component';
+import { OrdercompleteComponent } from './ordercomplete/ordercomplete.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    CartComponent,
     ProductComponent,
+    AdminComponent,
+    LoginComponent,
+    RegisterComponent,
+    FinalOrderComponent,
+    MyorderComponent,
+    CartComponent,
+    OrdercompleteComponent,
   ],
   imports: [
     BrowserModule,
