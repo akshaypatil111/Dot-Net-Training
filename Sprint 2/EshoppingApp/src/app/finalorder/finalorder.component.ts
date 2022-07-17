43d143606a8907890c311f55aed94f971f20ee0d
Akshay Patil <aksh.patil3@gmail.com>
2022-07-18 02:07:55 +0530

    commit message

--- Sprint 2/EshoppingApp/src/app/finalorder/finalorder.component.ts
+++ Sprint 2/EshoppingApp/src/app/finalorder/finalorder.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { finalorder } from '../models/finalorder';
+import { AdddetailService } from '../services/adddetail.service';
 
 @Component({
   selector: 'app-finalorder',
@@ -7,9 +11,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FinalOrderComponent implements OnInit {
 
-  constructor() { }
+  constructor(public httpc: HttpClient, private _detail: AdddetailService, private _router: Router) { }
 
   ngOnInit(): void {
   }
+
+  adddetail: finalorder = new finalorder();
+  adddetails: Array<finalorder> = new Array<finalorder>();
+
+  Adddetail() {
+    console.log(this.adddetail)
+
+    var deto = {
+      id: Number(this.adddetail.id),
+      firstName: this.adddetail.firstName,
+      lastName: this.adddetail.lastName,
+      address: this.adddetail.address,
+      state: this.adddetail.state,
+      city: this.adddetail.city,
+      zip: Number(this.adddetail.zip),
+      email: this.adddetail.email,
+      orderName: this.adddetail.orderName,
+      orderQuantity: Number(this.adddetail.orderQuantity),
+      orderPrice: Number(this.adddetail.orderPrice)
+    }
+    this.httpc.post("https://localhost:44321/api/FinalOrder", deto).subscribe(res => this.PostSuccess(res), res => this.PostError(res));
+    this.adddetail = new finalorder();
+
+
+  }
+
+  PostSuccess(res: any) {
+    console.log(res);
+
+  }
+  PostError(res: any) {
+    console.log(res);
+  }
+
+  Show() {
+    console.log("Hi");
+    this.httpc.get("https://localhost:44321/api/FinalOrder").subscribe(res => this.GetSuccess(res), res => this.GetError(res));
+  }
+  GetSuccess(input: any) {
+    this.adddetails = input;
+  }
+  GetError(input: any) {
+    console.log(input);
+  }
+
 
 }
